1cdbec947c11c55e529f2048d3e4fa55c69dcccd
Roy Engelstad <ren0056@arribatec.com>
2020-05-18 14:50:52 +0200

    "CREATE TABLE script from a select query" generator

---
+++ TailorTools/Pages/CreateFromSelect.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TailorTools.Pages
+{
+    public class CreateFromSelectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
