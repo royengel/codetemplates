e84d340dee3cf949839aab32c7d07202d1d40a16
Roy Engelstad <ren0056@arribatec.com>
2022-02-03 07:46:22 +0100

    .Net 6

--- TailorTools/Startup.cs
+++ TailorTools/Startup.cs
@@ -32,7 +32,7 @@ namespace TailorTools
 
             services.AddRazorPages();
 
-            services.AddMvc(o => o.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc(o => o.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
